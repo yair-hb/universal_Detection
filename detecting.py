e5b91a249e0ea6fd6e02bb0663a10fc632cf98a6
yair-hb <aythb@hotmail.com>
2022-02-08 21:24:51 -0600

    se corrigen algunos parametros para la deteccion de del objeto

--- detecting.py
+++ detecting.py
@@ -8,11 +8,11 @@ while True:
     ret, frame = captura.read()
     gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    objeto = cuadradoClassif.detectMultiScale(gris,scaleFactor=10, minNeighbors=50,minSize=(40,48),maxSize=(70,78))
+    objeto = cuadradoClassif.detectMultiScale(gris,scaleFactor=9,minNeighbors=55,minSize=(70,78))
 
     for (x,y,w,h) in objeto:
         cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-        cv2.putText(frame,'pikachu',(x,y-10),2,0.7,(0,255,0),2,cv2.LINE_AA)
+        cv2.putText(frame,'Mario',(x,y-10),2,0.7,(0,255,0),2,cv2.LINE_AA)
     
     cv2.imshow ('frame', frame)
     if cv2.waitKey(1) == 27:
